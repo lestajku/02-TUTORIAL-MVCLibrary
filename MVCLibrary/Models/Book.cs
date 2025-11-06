9e14de5a2c0b9778d92e83d51af9fd3d0231862d
lestajku <Student@LEONORA1>
2025-11-06 18:09:10 +0100

    prvizadatak

---
+++ MVCLibrary/Models/Book.cs
@@ -0,0 +1,12 @@
+﻿namespace MVCLibrary.Models
+{
+    public class Book
+    {
+
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string ISBN { get; set; }
+    }
+}
