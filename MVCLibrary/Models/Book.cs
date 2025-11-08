762228d035124b243328ae83d0ad55c371ba4aa9
lestajku <Student@LEONORA1>
2025-11-08 01:15:13 +0100

    02-tutorijal

--- MVCLibrary/Models/Book.cs
+++ MVCLibrary/Models/Book.cs
@@ -5,8 +5,10 @@
 
         public int Id { get; set; }
 
-        public string Title { get; set; }
+        public string Title { get; set; } = string.Empty;
 
-        public string ISBN { get; set; }
+        public string ISBN { get; set; } = string.Empty ;
+
+        public string Author { get; set; } = string.Empty;
     }
 }
